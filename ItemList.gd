77e886a608926ad1311b9fdeb0e8db0938506db6
Terkwood <metaterkhorn@gmail.com>
2019-03-30 10:37:04 -0400

    Initial commit

---
+++ ItemList.gd
@@ -0,0 +1,10 @@
+extends ItemList
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.add_item("Turkey", $TurkeyTexture.texture)
+	self.add_item("Turkey", $TurkeyTexture.texture)
+	self.add_item("Turkey", $TurkeyTexture.texture)
+	self.add_item("Turkey", $TurkeyTexture.texture)
+	pass # Replace with function body.
+
